f7e6e1168871fc3b6d66b07abd6338240060828e
planetoida90210 <galeriakoloru90210@gmail.com>
2022-01-28 00:00:46 +0100

    maping single todo and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import TodoList from './components/TodoList';
 import { Todo } from './model';
 
 
@@ -22,6 +23,7 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Taskmanager</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
