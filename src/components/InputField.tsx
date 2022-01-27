55b93f4242b64b9c9e82a0ad0eac09b35532c989
planetoida90210 <galeriakoloru90210@gmail.com>
2022-01-27 02:22:52 +0100

    add tasks to array by submiting button

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,11 +4,17 @@ import './styles.css'
 interface Props{
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField = ({todo,setTodo}: Props) => {
-  return <form className='input'>
-      <input type="input" placeholder="Enter a task" className='input__box'/>
+const InputField: React.FC<Props> = ({todo,setTodo, handleAdd}) => {
+  return <form className='input' onSubmit={handleAdd}>
+      <input 
+      type="input" 
+      value={todo} 
+      onChange={(e) => setTodo(e.target.value)} 
+      placeholder="Enter a task" 
+      className='input__box'/>
       <button className="input__submit" type="submit">
         Go
       </button>
