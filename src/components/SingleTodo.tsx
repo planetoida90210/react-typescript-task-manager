79507a05aaf11d2afbc1fcec1857ca19e3e584f5
planetoida90210 <galeriakoloru90210@gmail.com>
2022-01-28 02:56:55 +0100

    task complete funtionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import { Todo } from '../model';
 import {AiFillEdit, AiFillDelete} from 'react-icons/ai';
 import {MdDone} from 'react-icons/md';
 import "./styles.css";
+import TodoList from './TodoList';
 
 type Props = {
   todo:Todo,
@@ -11,12 +12,25 @@ type Props = {
 }
 
 const SingleTodo = ({todo,todos,setTodos}: Props) => {
+  
+  const handleDone = (id:number) => {
+      setTodos(todos.map((todo) => todo.id === id?{...todo, isDone:!todo.isDone}: todo)
+       ) };
+
   return <form className='todos__single'>
-    <span className="todos__single--text">{todo.todo}</span>
+    {
+      todo.isDone ? (
+        
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        
+        <span className="todos__single--text">{todo.todo}</span>
+      )
+    }
     <div>
       <span className="icon"><AiFillEdit/></span>
       <span className="icon"><AiFillDelete/></span>
-      <span className="icon"><MdDone/></span>
+      <span className="icon" onClick={()=> handleDone(todo.id)}><MdDone/></span>
     </div>
   </form>;
 };
