5b26c0850b2ab7e7ed6535cc9dbe4cb5448e9058
planetoida90210 <galeriakoloru90210@gmail.com>
2022-01-30 23:21:30 +0100

    add input ref

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Todo } from '../model';
 import {AiFillEdit, AiFillDelete} from 'react-icons/ai';
 import {MdDone} from 'react-icons/md';
 import "./styles.css";
-import TodoList from './TodoList';
 
 type Props = {
   todo:Todo,
@@ -32,12 +31,19 @@ const SingleTodo = ({todo,todos,setTodos}: Props) => {
       setEdit(false);
   };
 
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(()=> {
+    inputRef.current?.focus();
+  }, [edit])
+
+
   return (
 
   <form className='todos__single' onSubmit={(e)=> handleEdit(e,todo.id)}>
     {
       edit ? (
-          <input value={editTodo} onChange={(e) => setEditTodo(e.target.value)}/>
+          <input ref={inputRef} value={editTodo} onChange={(e) => setEditTodo(e.target.value)}/>
       ) : (
         
           todo.isDone ? (
